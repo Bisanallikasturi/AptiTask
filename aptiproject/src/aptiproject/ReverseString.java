741c41ce2624061521844b0f9eaf1d7557b93305
Bisanallikasturi <60281911+Bisanallikasturi@users.noreply.github.com>
2020-02-27 22:37:26 +0530

    Add files via upload

---
+++ aptiproject/src/aptiproject/ReverseString.java
@@ -0,0 +1,19 @@
+package aptiproject;
+
+public class ReverseString {
+	public static String reverseWord(String str){  
+	    String words[]=str.split("\\s");  
+	    String reverseWord="";  
+	    for(String w:words){  
+	        StringBuilder sb=new StringBuilder(w);  
+	        sb.reverse();  
+	        reverseWord+=sb.toString()+" ";  
+	    }  
+	    return reverseWord.trim();  
+	}  
+	
+	public static void main(String[] args) {  
+	    System.out.println(ReverseString.reverseWord("kasturi"));  
+	    System.out.println(ReverseString.reverseWord("Manjunath"));    
+	    }  
+}
